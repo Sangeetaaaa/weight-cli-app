71b39ec3621dbca366dd061e10d48ce3eaacf742
Sangeeta <saangeeeeta@gmail.com>
2021-01-06 19:08:37 +0530

    final

--- src/commands/tp.js
+++ src/commands/tp.js
@@ -1,21 +1,26 @@
 const {Command, flags} = require('@oclif/command')
-const { execFile } = require('child_process'); 
+const exec = require('child_process').exec; 
+var cmd=require('node-cmd'); 
+const process = require('process'); 
+// const file = require('./my.bat')
 // const fixPath = require('fix-path');
 // fixPath();
 
-let cmd = 'cd ..'
 
 class TpCommand extends Command {
   async run() {
     const {flags} = this.parse(TpCommand)
-// exec('cd c/Users/DELL/Documents/Web\ Dev\ Projects/weight/', (error, stdout, stderr) => {
-  const child = execFile('node', ['my.txt'],  
-        (error, stdout, stderr) => { 
+  
+
+exec(`touch sangeeta.txt`, (error, stdout, stderr) => { 
   if (error) { 
-    throw error; 
+    console.error(`exec error: ${error}`); 
+    return; 
   } 
-  console.log(stdout); 
-});   
+  console.log(`stdout: No. of directories = ${stdout}`); 
+  if (stderr!= "") 
+  console.error(`stderr: ${stderr}`); 
+}); 
   }
 }
 
